b6c2b3506fd4ce80249900b896bb46f0cd676ced
Eben <e2kelly2000@gmail.com>
2020-09-03 05:08:45 +0100

    initialized homepage with little styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Homepage from './homepage.component'
 
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+    return (
+      <div className='App'>
+        <Homepage />
+      </div>
+    )
   }
 }
 
