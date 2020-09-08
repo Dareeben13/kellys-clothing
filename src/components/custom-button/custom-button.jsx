99db85bcbb2f8052d1f68086dad3251c792ff453
Eben <e2kelly2000@gmail.com>
2020-09-08 03:30:42 +0100

    updated shop data with new data , and updated cart utils to group items in cartReducer

--- src/components/custom-button/custom-button.jsx
+++ src/components/custom-button/custom-button.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 import "./custom-button.scss";
 
-function CustomButton({ children, isGoogleSignIn, ...otherProps }) {
+function CustomButton({ children, isGoogleSignIn, inverted, ...otherProps }) {
   return (
     <button
-      className={`${isGoogleSignIn ? "google-sign-in" : " "} custom-button`}
+      className={`${inverted ? "inverted" : " "}${
+        isGoogleSignIn ? "google-sign-in" : " "
+      } custom-button`}
       {...otherProps}
     >
       {children}
--- src/components/preview-collection/collection-preview.jsx
+++ src/components/preview-collection/collection-preview.jsx
@@ -10,9 +10,11 @@ function CollectionPreview({ title, items }) {
       <h1 className="title">{title.toUpperCase()}</h1>
       <div className="preview">
         {items
-          .filter((item, idx) => idx < 4)
-          .map(({ id, ...otherItemProps }) => (
-            <CollectionItem key={id} {...otherItemProps} />
+          .filter((item, idx) => idx < 4) // filter is just making sure that items on each row is just 4 i.e 
+          // till index 4
+          .map(item => ( // WE are just passing the entire item i.e CollectionItem back into itself, so that
+            // It can be accessible in Its own Component
+            <CollectionItem key={item.id} item={item}/>
           ))}
       </div>
     </div>
