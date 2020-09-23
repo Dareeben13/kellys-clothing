0b1d67d3ffdfdde921b3f948c0baa9a6f23ba074
Dareeben13 <e2kelly2000@gmail.com>
2020-09-23 17:52:33 +0100

    stripe backend enabled

---
+++ client/build/precache-manifest.eb9a47120c5b7b8297ea6596a0c86e90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc10724281a8816d0bc9",
+    "url": "/static/css/main.06f10933.chunk.css"
+  },
+  {
+    "revision": "bc10724281a8816d0bc9",
+    "url": "/static/js/main.878eb61f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6fcb6e3faf62f0468d75",
+    "url": "/static/js/2.a85506a9.chunk.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "92ca9732460590b07e461d2ffc34b994",
+    "url": "/index.html"
+  }
+];
