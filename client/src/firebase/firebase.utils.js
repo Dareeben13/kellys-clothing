af78d0efcbff60113eb21e0b01761866642d85ca
Dareeben13 <e2kelly2000@gmail.com>
2020-10-15 18:51:30 +0100

    testing sign in with google

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -103,12 +103,7 @@ export const firestore = firebase.firestore()
 
 
 export const googleProvider = new firebase.auth.GoogleAuthProvider();
-
-
-
-
 googleProvider.setCustomParameters({ prompt: 'select_account' });
-
 export const signInWithGoogle = () => auth.signInWithPopup(googleProvider)
 
 export default firebase;
